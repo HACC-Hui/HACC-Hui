e4b04f411be6c91625be4576fc9971c3e48614ee
Cam Moore <cammymoore07@gmail.com>
2020-06-19 12:09:47 -1000

    Update App.jsx
    Added subscriptions to the protected routes.

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -14,6 +14,7 @@ import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signout from '../pages/Signout';
+import withAllSubscriptions from './AllSubscriptionsHOC';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -44,37 +45,43 @@ class App extends React.Component {
  * Checks for Meteor login before routing to the requested page, otherwise goes to signin page.
  * @param {any} { component: Component, ...rest }
  */
-const ProtectedRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) => {
-      const isLogged = Meteor.userId() !== null;
-      return isLogged ?
-          (<Component {...props} />) :
-          (<Redirect to={{ pathname: '/signin', state: { from: props.location } }}/>
-      );
-    }}
-  />
-);
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const WrappedComponent = withAllSubscriptions(Component);
+  return (
+      <Route
+          {...rest}
+          render={(props) => {
+            const isLogged = Meteor.userId() !== null;
+            return isLogged ?
+                (<WrappedComponent {...props} />) :
+                (<Redirect to={{ pathname: '/signin', state: { from: props.location } }}/>
+                );
+          }}
+      />
+  );
+};
 
 /**
  * AdminProtectedRoute (see React Router v4 sample)
  * Checks for Meteor login and admin role before routing to the requested page, otherwise goes to signin page.
  * @param {any} { component: Component, ...rest }
  */
-const AdminProtectedRoute = ({ component: Component, ...rest }) => (
-    <Route
-        {...rest}
-        render={(props) => {
-          const isLogged = Meteor.userId() !== null;
-          const isAdmin = Roles.userIsInRole(Meteor.userId(), 'admin');
-          return (isLogged && isAdmin) ?
-              (<Component {...props} />) :
-              (<Redirect to={{ pathname: '/signin', state: { from: props.location } }}/>
-              );
-        }}
-    />
-);
+const AdminProtectedRoute = ({ component: Component, ...rest }) => {
+  const WrappedComponent = withAllSubscriptions(Component);
+  return (
+      <Route
+          {...rest}
+          render={(props) => {
+            const isLogged = Meteor.userId() !== null;
+            const isAdmin = Roles.userIsInRole(Meteor.userId(), 'admin');
+            return (isLogged && isAdmin) ?
+                (<WrappedComponent {...props} />) :
+                (<Redirect to={{ pathname: '/signin', state: { from: props.location } }}/>
+                );
+          }}
+      />
+  );
+};
 
 /** Require a component and location to be passed to each ProtectedRoute. */
 ProtectedRoute.propTypes = {
