34d0bfb27b21074496f68739a0210f0246a3aac3
angeli-amascual <aamascua@hawaii.edu>
2020-09-01 11:14:48 -1000

    Combined Mockup

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Container, Image } from 'semantic-ui-react';
+import { Grid, Image } from 'semantic-ui-react';
 
 /**
  * A simple static component to render some text for the landing page.
@@ -8,42 +8,64 @@ import { Grid, Container, Image } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
-          <Grid columns={'equal'}>
-              <Grid.Column width={8} >
-                <Grid.Row style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: 20 }}>
-                <h1 style={{ color: '#0B2C52' }}>Welcome to HACC-Hui</h1>
-                <p style={{ paddingLeft: 20, color: '#0B2C52' }}>
-                  Our goal is to simplify team formation and ongoing team management
-                  for the Hawaii Annual Code Challenge.
-                  <br />
-                  <br />
-                  Here you can create a new team or join an already made one.
-                  Our application can help you find the perfect team for you, or help you look for members that fit your team’s requirements.
-                </p>
-                </Grid.Row>
-                <Grid rows={'equal'}>
-                <Grid.Row style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', margin: 10, paddingTop: 20, paddingLeft: 50, paddingRight: 50 }}>
-                  <Grid.Column width={8} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', height: '100%' }}>
-                    <Image size='mini' src="/images/slack.png"/>
-                    <b style={{ color: '#0B2C52' }}>Slack Support</b>
-                    <p style={{ color: '#0B2C52' }}>
-                      Contact teams you’re interest in joining, or users you are interested in inviting.
-                    </p>
-                  </Grid.Column>
-                  <Grid.Column width={8} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', height: '100%' }}>
-                    <Image size='mini' src="/images/document.png"/>
-                    <b style={{ color: '#0B2C52' }}>Member Profile</b>
-                    <p style={{ color: '#0B2C52' }}>
-                      Create a detailed member profile to show propspective teams your skills or view user profiles to find the best teammate for your team.
-                    </p>
-                  </Grid.Column>
-                </Grid.Row>
-                </Grid>
+        <Grid rows={'equal'}>
+          <Grid.Row style={{ margin: 50 }}>
+              <Grid.Column width={8} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                  <b style={{ color: '#0B2C52', fontSize: 30, alignSelf: 'flex-start', paddingLeft: 20, marginBottom: 15 }}>Welcome to HACC-Hui</b>
+                  <p style={{ fontSize: 20, paddingLeft: 20, color: '#0B2C52' }}>
+                    Our goal is to simplify team formation and ongoing team management
+                    for the Hawaii Annual Code Challenge.
+                    <br />
+                    <br />
+                    Here you can create a new team or join an already made one.
+                    Our application can help you find the perfect team for you, or help you look for members that fit your team’s requirements.
+                  </p>
               </Grid.Column>
-            <Grid.Column width={8} centered style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <Grid.Column width={8} centered style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <Image centered size='medium' src="/images/hacc.png"/>
+              </Grid.Column>
+          </Grid.Row>
+          <Grid.Row style={{ backgroundColor: '#E5F0FE' }}>
+            <Grid.Column width={8} centered style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center' }}>
+              <Image centered size='small' src="/images/profile8.png"/>
+              <b style={{ fontSize: 30, fontStyle: 'italic', margin: 15 }}>
+                Develop your profile
+              </b>
+              <p style={{ fontSize: 20, fontStyle: 'italic' }}>
+                Create your profile to participate in HACC
+              </p>
             </Grid.Column>
-          </Grid>
+            <Grid.Column width={8} centered style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center' }}>
+              <Image centered size='small' src="/images/team.png"/>
+              <b style={{ fontSize: 30, fontStyle: 'italic', margin: 15 }}>
+                Create a team
+              </b>
+              <p style={{ fontSize: 20, fontStyle: 'italic' }}>
+                Create your team to solve a challenge and win the HACC
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row style={{ backgroundColor: '#E5F0FE' }}>
+            <Grid.Column width={8} centered style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center' }}>
+              <Image centered size='small' src="/images/join.png"/>
+              <b style={{ fontSize: 30, fontStyle: 'italic', margin: 15 }}>
+                Join a team
+              </b>
+              <p style={{ fontSize: 20, fontStyle: 'italic' }}>
+                Find a team to join and tackle a challenge together
+              </p>
+            </Grid.Column>
+            <Grid.Column width={8} centered style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center' }}>
+              <Image centered size='small' src="/images/slackicon.png"/>
+              <b style={{ fontSize: 30, fontStyle: 'italic', margin: 15 }}>
+                Utilize Slack
+              </b>
+              <p style={{ fontSize: 20, fontStyle: 'italic' }}>
+                Communicate with your team through Slack
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
     );
   }
 }
