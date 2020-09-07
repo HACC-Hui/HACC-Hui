b0fc6a2dd1483f3e7120d34cb8edb4aabdc414c9
nzenger <nzenger@hawaii.edu>
2020-09-06 17:25:07 -1000

    added account options page for viewing info and changing accoutn status

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -14,6 +14,7 @@ import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signout from '../pages/Signout';
+import AccountOptions from '../pages/AccountOptions';
 import withAllSubscriptions from './AllSubscriptionsHOC';
 import { ROLE } from '../../api/role/Role';
 
@@ -35,6 +36,7 @@ class App extends React.Component {
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
               <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
               <ProtectedRoute path="/signout" component={Signout}/>
+                <ProtectedRoute path="/accountoptions" component={AccountOptions}/>
               <Route component={NotFound}/>
             </Switch>
             <Footer/>
