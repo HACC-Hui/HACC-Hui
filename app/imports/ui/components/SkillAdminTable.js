63272dda1a495cc9c8f6b8b8c15170e58c8bdf4a
Kelli Tamashiro <kellikt@hawaii.edu>
2020-09-09 22:39:22 -1000

    Removing skills work

--- app/imports/ui/components/SkillAdminTable.js
+++ app/imports/ui/components/SkillAdminTable.js
@@ -2,9 +2,33 @@ import React from 'react';
 import { Button, Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { Link, withRouter } from 'react-router-dom';
+import swal from 'sweetalert';
+import { removeItMethod } from '../../api/base/BaseCollection.methods';
+import { Skills } from '../../api/skill/SkillCollection';
 
 /** Renders a single row in the table. See pages/Listmenuitemss.jsx. */
 class SkillAdminTable extends React.Component {
+  removeItem(docID) {
+    swal({
+      title: 'Are you sure?',
+      text: 'Once deleted, you will not be able to recover this skill!',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    })
+        .then((willDelete) => {
+          if (willDelete) {
+            removeItMethod.call({
+              collectionName: Skills.getCollectionName(),
+              instance: Skills.getID(docID),
+            }, (error) => (error ?
+                swal('Error', error.message, 'error') :
+                swal('Success', 'Skill removed', 'success')));
+          } else {
+            swal('You canceled the deletion!');
+          }
+        });
+  }
 
   render() {
     return (
@@ -13,7 +37,8 @@ class SkillAdminTable extends React.Component {
           <Table.Cell>{this.props.skills.description}</Table.Cell>
           {/* eslint-disable-next-line max-len */}
           <Table.Cell width={2}><Button><Link to={`/edit-skill/${this.props.skills._id}`} style={{ color: 'rgba(0, 0, 0, 0.6)' }}>Edit</Link></Button></Table.Cell>
-          <Table.Cell width={2}><Button negative>Delete</Button></Table.Cell>
+          {/* eslint-disable-next-line max-len */}
+          <Table.Cell width={2}><Button negative onClick={() => this.removeItem(this.props.skills._id)}>Delete</Button></Table.Cell>
         </Table.Row>
     );
   }
