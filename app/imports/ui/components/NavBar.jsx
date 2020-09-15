4fedb7f034085137e21f13bc3be3ec876bfdedca
Cam Moore <cammymoore07@gmail.com>
2020-09-15 10:22:48 -1000

    Update NavBar.jsx

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -44,7 +44,7 @@ class NavBar extends React.Component {
                 <Dropdown text={this.props.currentUser} pointing="top right" icon={'user'}>
                   <Dropdown.Menu>
                     <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to={ROUTES.SIGN_OUT} />
-                    {Roles.userIsInRole(Meteor.userId(), ROLE.DEVELOPER) ? (
+                    {isDeveloper ? (
                         <Dropdown.Item icon="user delete" text="Delete Account" as={NavLink} exact
                                        to={ROUTES.DELETE_ACCOUNT} />) : ''}
                   </Dropdown.Menu>
