1bbc4e0f58932d1711f5ad53c0f935edd8e19648
Kelli Tamashiro <kellikt@hawaii.edu>
2020-09-30 20:17:22 -1000

    Created TeamInvitationCollection and draft of AllDevelopers UI

---
+++ app/imports/ui/components/AllDevelopersCard.jsx
@@ -0,0 +1,164 @@
+import React from 'react';
+import {
+  Grid,
+  Header,
+  Item,
+  Modal,
+  Icon, Button,
+} from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import swal from 'sweetalert';
+import { defineMethod } from '../../api/base/BaseCollection.methods';
+import { TeamInvitations } from '../../api/team/TeamInvitationCollection';
+
+class AllDevelopersCard extends React.Component {
+  /*
+  isAdded(tID, dID) {
+    // console.log(typeof TeamDevelopers.findOne({ teamID: tID, developerID: dID }) !== 'undefined');
+    if (typeof TeamDevelopers.findOne({ teamID: tID, developerID: dID }) !== 'undefined') {
+      return true;
+    }
+    return false;
+  }
+   */
+
+  handleClick(tID, dID, e) {
+    console.log(e);
+    // console.log(tID);
+    // console.log(dID);
+    const thisTeam = tID;
+    const devID = dID;
+    // console.log(thisTeam);
+    const definitionData = { team: thisTeam, developer: devID };
+    const collectionName = TeamInvitations.getCollectionName();
+    // console.log(collectionName);
+    defineMethod.call({ collectionName: collectionName, definitionData: definitionData },
+        (error) => {
+          if (error) {
+            swal('Error', error.message, 'error');
+            // console.error(error.message);
+          } else {
+            swal('Success', 'Invitation sent successfully', 'success');
+            // console.log('Success');
+          }
+        });
+  }
+
+  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  render() {
+
+    function changeBackground(e) {
+      e.currentTarget.style.backgroundColor = '#fafafa';
+      e.currentTarget.style.cursor = 'pointer';
+    }
+
+    function onLeave(e) {
+      e.currentTarget.style.backgroundColor = 'transparent';
+    }
+
+    return (
+        <Item onMouseEnter={changeBackground} onMouseLeave={onLeave}
+              style={{ padding: '0rem 2rem 0rem 2rem' }}>
+          <Modal closeIcon trigger={
+            <Item.Content>
+              <Item.Header>
+                <Header as={'h3'} style={{ color: '#263763', paddingTop: '2rem' }}>
+                  <Icon name='user' size='tiny' />
+                  {this.props.developers.firstName} {this.props.developers.lastName}
+                </Header>
+              </Item.Header>
+              <Item.Meta>
+                <Item.Meta>
+                  <Grid doubling columns={5}>
+                    <Grid.Column>
+                      <Grid.Column floated={'left'} style={{ paddingBottom: '0.3rem' }}>
+                        {this.props.challenges.map((challenge) => <p
+                            style={{ color: 'rgb(89, 119, 199)' }}
+                            key={challenge}>
+                          {challenge}</p>)}
+                      </Grid.Column>
+
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Header>Skills</Header>
+                      {this.props.skills.map((skill) => <p key={skill}>
+                        {skill.name}</p>)}
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Header>Tools</Header>
+                      {this.props.tools.map((tool) => <p key={tool}>
+                        {tool.name}</p>)}
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Header>Slack Username</Header>
+                      {this.props.developers.username}
+                    </Grid.Column>
+                  </Grid>
+                </Item.Meta>
+              </Item.Meta>
+
+            </Item.Content>
+          }>
+            <Modal.Header>{this.props.developers.firstName} {this.props.developers.lastName}</Modal.Header>
+            <Modal.Content image scrolling>
+              <Modal.Description>
+                <Header>About Me</Header>
+                <p>
+                  {this.props.developers.aboutMe}
+                </p>
+                <Header>Slack Username</Header>
+                <p>
+                {this.props.developers.username}
+                </p>
+                <Header>LinkedIn</Header>
+                <p>
+                  {this.props.developers.linkedIn}
+                </p>
+                <Header>GitHub</Header>
+                <p>
+                  {this.props.developers.gitHub}
+                </p>
+                <Header>Website</Header>
+                <p>
+                  {this.props.developers.website}
+                </p>
+                <Header>Challenges</Header>
+                <p>
+                  {this.props.challenges.map((challenge) => <p key={challenge}>
+                    {challenge}</p>)}
+                </p>
+                <Header>Skills</Header>
+                <p>
+                  {this.props.skills.map((skill) => <p key={skill}>
+                    {skill.name}: {skill.level}</p>)}
+                </p>
+                <Header>Tools</Header>
+                <p>
+                  {this.props.tools.map((tool) => <p key={tool}>
+                    {tool.name}: {tool.level}</p>)}
+                </p>
+              </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button style={{ backgroundColor: 'rgb(89, 119, 199)', color: 'white' }}>
+                <Icon name='plus'/>
+                Send Invitation
+              </Button>
+            </Modal.Actions>
+          </Modal>
+          <Button style={{ backgroundColor: 'transparent' }}>
+            Send Invitation
+          </Button>
+        </Item>
+    );
+  }
+}
+
+AllDevelopersCard.propTypes = {
+  skills: PropTypes.array.isRequired,
+  tools: PropTypes.array.isRequired,
+  challenges: PropTypes.array.isRequired,
+  developers: PropTypes.object.isRequired,
+};
+
+export default AllDevelopersCard;
