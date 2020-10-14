71b9d03cad84f4c1eb9e85ce124c17b2ad6ecaca
Jenny <jhsu2@hawaii.edu>
2020-10-13 17:54:17 -1000

    Removed sidebar from App.jsx

--- app/imports/ui/components/SideBar.jsx
+++ app/imports/ui/components/SideBar.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Header, Sidebar, Segment } from 'semantic-ui-react';
+import { Menu, Header, Sidebar, Segment, Icon } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
 import { ROLE } from '../../api/role/Role';
 import { ROUTES } from '../../startup/client/route-constants';
@@ -13,6 +13,12 @@ import { ROUTES } from '../../startup/client/route-constants';
  * @memberOf ui/components
  */
 class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
 
   render() {
 
@@ -20,62 +26,88 @@ class SideBar extends React.Component {
     const isParticipant = this.props.currentUser && Roles.userIsInRole(Meteor.userId(), ROLE.PARTICIPANT);
     // console.log(isParticipant);
 
-    return (
+    const setVisible = (state) => {
+      this.setState({ visible: state });
+    };
 
-        <Sidebar.Pushable as={Segment} className={'sideBar'}>
-          <Sidebar
-              style={{ paddingTop: '4rem' }}
-              as={Menu}
-              animation='overlay'
-              icon='labeled'
-              inverted
-              vertical
-              visible={this.props.visible}
-              width='thin'
-          >
-            <Menu.Item as={NavLink} activeClassName="" exact to={ROUTES.LANDING}>
-              <Header inverted as='h1'>HACC-Hui</Header>
+    return (
+        <div>
+          <Menu borderless inverted fixed={'top'} className={'mobileBar'}>
+            <Menu.Item position={'left'}>
+              <div onClick={() => setVisible(!this.state.visible)} style={{ padding: '5px' }}>
+                <Icon name='bars'/>
+              </div>
             </Menu.Item>
-            {isParticipant ? (
-                [<Menu.Item as={NavLink} activeClassName="active" exact
-                            to={ROUTES.CREATE_TEAM} key='team-creation'>Create a Team</Menu.Item>,
-                  <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.YOUR_PROFILE}
-                             key='edit-profile'>
-                    Your Profile</Menu.Item>,
-                  <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.LIST_TEAMS}
-                             key='list-teams'>List the
-                    Teams</Menu.Item>,
-                  <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.YOUR_TEAMS}
-                             key='your-teams'>Your
-                    Teams</Menu.Item>,
-                  // eslint-disable-next-line max-len
-                  <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.LIST_DEVELOPERS}
-                             key='list-participants'>List the
-                    Participants</Menu.Item>,
-                ]
-            ) : ''}
-            {isAdmin ? (
-                [
-                  <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.CONFIGURE_HACC}
-                             key={ROUTES.CONFIGURE_HACC}>Configure HACC</Menu.Item>,
-                  <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.DUMP_DATABASE}
-                             key={ROUTES.DUMP_DATABASE}>Dump Database</Menu.Item>,
-                ]
-            ) : ''}
-            <Menu.Item>
-              {this.props.currentUser === '' ? (
-                  <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.SIGN_IN}
-                             key={ROUTES.SIGN_IN}>Sign In</Menu.Item>
-              ) : (
-                  <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.SIGN_OUT}
-                             key={ROUTES.SIGN_OUT}>Sign Out</Menu.Item>
-              )}
-            </Menu.Item>
-          </Sidebar>
-          <Sidebar.Pusher>
-            {this.props.children}
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
+          </Menu>
+          <Sidebar.Pushable as={Segment} className={'sideBar'}>
+            <Sidebar
+                style={{ paddingTop: '4rem' }}
+                as={Menu}
+                animation='overlay'
+                icon='labeled'
+                inverted
+                vertical
+                visible={this.state.visible}
+                width='thin'
+            >
+              <Menu.Item as={NavLink} activeClassName="" exact to={ROUTES.LANDING}
+                         onClick={() => setVisible(!this.state.visible)}>
+                <Header inverted as='h1'>HACC-Hui</Header>
+              </Menu.Item>
+              {isParticipant ? (
+                  [<Menu.Item as={NavLink} activeClassName="active"
+                              onClick={() => setVisible(!this.state.visible)}
+                              exact to={ROUTES.CREATE_TEAM} key='team-creation'>Create a
+                    Team</Menu.Item>,
+                    <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.YOUR_PROFILE}
+                               key='edit-profile'
+                               onClick={() => setVisible(!this.state.visible)}>
+                      Your Profile</Menu.Item>,
+                    <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.LIST_TEAMS}
+                               key='list-teams'
+                               onClick={() => setVisible(!this.state.visible)}>List the
+                      Teams</Menu.Item>,
+                    <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.YOUR_TEAMS}
+                               key='your-teams'
+                               onClick={() => setVisible(!this.state.visible)}>Your
+                      Teams</Menu.Item>,
+                    // eslint-disable-next-line max-len
+                    <Menu.Item as={NavLink} activeClassName="active" exact
+                               to={ROUTES.LIST_DEVELOPERS}
+                               key='list-participants'
+                               onClick={() => setVisible(!this.state.visible)}>List the
+                      Participants</Menu.Item>,
+                  ]
+              ) : ''}
+              {isAdmin ? (
+                  [
+                    <Menu.Item as={NavLink} activeClassName="active" exact
+                               to={ROUTES.CONFIGURE_HACC}
+                               key={ROUTES.CONFIGURE_HACC}
+                               onClick={() => setVisible(!this.state.visible)}>Configure HACC</Menu.Item>,
+                    <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.DUMP_DATABASE}
+                               key={ROUTES.DUMP_DATABASE}
+                               onClick={() => setVisible(!this.state.visible)}>Dump Database</Menu.Item>,
+                  ]
+              ) : ''}
+              <Menu.Item>
+                {this.props.currentUser === '' ? (
+                    <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.SIGN_IN}
+                               key={ROUTES.SIGN_IN}
+                               onClick={() => setVisible(!this.state.visible)}>Sign In</Menu.Item>
+                ) : (
+                    <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.SIGN_OUT}
+                               key={ROUTES.SIGN_OUT}
+                               onClick={() => setVisible(!this.state.visible)}>Sign Out</Menu.Item>
+                )}
+              </Menu.Item>
+            </Sidebar>
+            <Sidebar.Pusher style={{ paddingTop: '5rem' }}>
+              {this.props.children}
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
+        </div>
+
     );
   }
 }
