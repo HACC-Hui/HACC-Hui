aeaf4d86f5fba16cc49b9ce55adee3f692cf0907
brysonsy <60155993+brysonsy@users.noreply.github.com>
2020-10-14 20:03:50 -1000

    Changed color to more fitting one.

--- app/imports/ui/components/participant/CreateTeamWidget.jsx
+++ app/imports/ui/components/participant/CreateTeamWidget.jsx
@@ -153,7 +153,7 @@ class CreateTeamWidget extends React.Component {
                       }}>
               <Segment style={{
                 borderRadius: '10px',
-                backgroundColor: '#BDEDFF',
+                backgroundColor: '#9AFEFF',
               }} className={'createTeam'}>
                 <Grid columns={1} style={{ paddingTop: '20px' }}>
                   <Grid.Column style={{ paddingLeft: '30px', paddingRight: '30px' }}>
