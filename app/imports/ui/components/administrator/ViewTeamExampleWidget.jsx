c8ada800e81f59bbc8e81e663c56c70ba03886dc
justinhwong <justinhw@hawaii.edu>
2020-10-16 22:17:44 -1000

    Functionality for compliance

--- app/imports/ui/components/administrator/ViewTeamExampleWidget.jsx
+++ app/imports/ui/components/administrator/ViewTeamExampleWidget.jsx
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Form, Grid, Header, List, } from 'semantic-ui-react';
+import { Grid, Header, List, } from 'semantic-ui-react';
 import _ from 'lodash';
 import { withTracker } from 'meteor/react-meteor-data';
 import { WantsToJoin } from '../../../api/team/WantToJoinCollection';
@@ -17,10 +17,8 @@ class ViewTeamExampleWidget extends React.Component {
     const collectionName = WantsToJoin.getCollectionName();
     const teamDoc = Teams.findDoc(inst.id);
     const team = Slugs.getNameFromID(teamDoc.slugID);
-    const participant = Participants.findDoc({ userID: Meteor.userId() }).username;
     const definitionData = {
       team,
-      participant,
     };
     console.log(collectionName, definitionData);
     defineMethod.call({ collectionName, definitionData }, (error) => {
@@ -30,14 +28,7 @@ class ViewTeamExampleWidget extends React.Component {
     });
   }
 
-  // checkCompliance() {
-  //   // const compliantParticipants = _.map(Participants.findDoc({ userID: Meteor.userId() }));
-  //   //create a function that maps through all the members of a team
-  //   //if all members of the team .isCompliant return: "Team is compliant"
-  //   //else return "Team is not compliant"
-
   render() {
-
     const allParticipants = this.props.participants;
     function getTeamParticipants(teamID, teamParticipants) {
       const data = [];
@@ -46,28 +37,14 @@ class ViewTeamExampleWidget extends React.Component {
         for (let j = 0; j < allParticipants.length; j++) {
           if (participants[i].participantID === allParticipants[j]._id) {
             data.push({
-              firstName: allParticipants[j].firstName,
-              lastName: allParticipants[j].lastName,
               compliant: allParticipants[j].isCompliant,
             });
           }
         }
       }
       return data;
-    }
-
-    console.log(this.props.team._id);
-    console.log(getTeamParticipants(this.props.team._id, this.props.teamParticipants));
-
-    // const part = Participants.findDoc({ userID: participantId });
-
-    //if (getTeamParticipants(teams._id, this.props.teamParticipants).isCompliant) {
-    //  console.log("compliant");
-    //} else {
-     // console.log("not");
-   // }
-    return (
-          <Grid celled>
+    }return (
+        <Grid celled>
           <Grid.Row columns={3}>
             <Grid.Column>
               <Header>{this.props.team.name}</Header>
@@ -78,32 +55,22 @@ class ViewTeamExampleWidget extends React.Component {
               </List>
             </Grid.Column>
             <Grid.Column>
-             { (getTeamParticipants(this.props.team._id, this.props.teamParticipants).isCompliant === true) ? <Header>
-                Team is Compliant.
-               </Header> : '' }
             </Grid.Column>
           </Grid.Row>
-          </Grid>
+        </Grid>
     );
   }
 }
 
 ViewTeamExampleWidget.propTypes = {
   team: PropTypes.object.isRequired,
-  participant: PropTypes.object.isRequired,
   participants: PropTypes.array.isRequired,
   teamParticipants: PropTypes.arrayOf(PropTypes.object).isRequired,
-  teamChallenges: PropTypes.arrayOf(
-      PropTypes.string,
-  ).isRequired,
-  teamSkills: PropTypes.arrayOf(
-      PropTypes.string,
-  ).isRequired,
-  teamTools: PropTypes.arrayOf(
-      PropTypes.string,
-  ).isRequired,
   teamMembers: PropTypes.arrayOf(
       PropTypes.string,
+  ).isRequired,
+  teamCompliance: PropTypes.arrayOf(
+      PropTypes.boolean,
   ).isRequired,
 };
 
