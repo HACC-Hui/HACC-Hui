c97fc9d094328791cc2fe730e536463c71825cbd
Moseli Motsoehli <u12061019@tuks.co.za>
2020-11-03 17:48:53 +0200

    Done with UI
    Changed ui to look better and be mobile friendly

--- app/imports/ui/pages/HelpPage.jsx
+++ app/imports/ui/pages/HelpPage.jsx
@@ -1,54 +1,95 @@
 import React from 'react';
-import { Header, Divider } from 'semantic-ui-react';
+import { Header, Divider, Grid, Container, Accordion, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class HelpPage extends React.Component {
+  state = { activeIndex: 0 }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  }
+
   render() {
+    const { activeIndex } = this.state;
     return (
-      <div style={{ textAlign: 'center' }}>
-        <p
-          style={{
+      <Container stackable>
+      <Header as='h1' textAlign='center' style={{
             fontSize: '40px',
             paddingTop: '20px',
-          }}
+          }}>
+        Questions By Category
+      </Header>
+      <Grid divided='vertically' stackable>
+      <Grid.Row columns={3}>
+        <Grid.Column>
+          <Divider horizontal style={{ paddingTop: '40px' }}>GENERAL</Divider>
+          <Accordion fluid styled exclusive={false}>
+            <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.handleClick}
+            >
+            <Icon name='dropdown' />
+            <Header as='h1' textAlign='center'>
+              <b>How do I Register?</b>
+            </Header>
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            <Header as='h2' textAlign='left'>
+            <a href={'https://slack.com/signin#/signin'}>
+              Join The Slack Workspace
+            </a>
+            </Header>
+            <p>
+              <b>
+                You will need to make a Slack account if you do not have a
+                pre-existing one <br></br> Join the Slack Workspace and type
+                &apos;register&apos; <br></br> You will then be given a username and password
+                to login.
+              </b>
+            </p>
+        </Accordion.Content>
+        <Accordion.Title
+          active={activeIndex === 1}
+          index={1}
+          onClick={this.handleClick}
         >
-          Questions By Category
-        </p>
-        <Divider horizontal>GENERAL</Divider>
-        <Header as='h1' textAlign='center'>
-          <b>How do I Register?</b>
-        </Header>
-        <Header as='h2' textAlign='center'>
-          <a href={'https://slack.com/signin#/signin'}>
-            Join The Slack Workspace
-          </a>
-        </Header>
-        <p>
-          <b>
-            You will need to make a Slack account if you do not have a
-            pre-existing one <br></br> Join the Slack Workspace and type
-            &apos;register&apos; <br></br> You will then be given a username and password
-            to login.
-          </b>
-        </p>
-        <div style={{ paddingTop: '40px' }}>
+          <Icon name='dropdown' />
           <Header as='h1' textAlign='center'>
             <b>What is HACC HUI?</b>
           </Header>
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 1}>
           <h4>
             <b>
               HACC HUI is an official HACC 2020 site to help participants create
               and manage their teams
             </b>
           </h4>
-        </div>
+        </Accordion.Content>
+        </Accordion>
+        </Grid.Column>
+        <Grid.Column>
         <Divider horizontal style={{ paddingTop: '40px' }}>
           TEAM MANAGEMENT
         </Divider>
-        <Header as='h1' textAlign='center'>
-          <b>Where can I find Teammates?</b>
-        </Header>
-        <Header as='h2' textAlign='center'>
+        <Accordion fluid styled exclusive={false}>
+        <Accordion.Title
+          active={activeIndex === 2}
+          index={2}
+          onClick={this.handleClick}
+          >
+          <Icon name='dropdown' />
+          <Header as='h1' textAlign='center'>
+            <b>Where can I find Teammates?</b>
+          </Header>
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 2}>
+        <Header as='h2' textAlign='left'>
           <Link to='list-participants'>
             <text>List Participants Page</text>
           </Link>
@@ -59,11 +100,19 @@ class HelpPage extends React.Component {
             page!
           </b>
         </p>
-        <div style={{ paddingTop: '40px' }}>
+        </Accordion.Content>
+        <Accordion.Title
+          active={activeIndex === 3}
+          index={3}
+          onClick={this.handleClick}
+          >
+          <Icon name='dropdown' />
           <Header as='h1' textAlign='center'>
             <b>How do I Leave/Delete my Team?</b>
           </Header>
-          <Header as='h2' textAlign='center'>
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 3}>
+          <Header as='h2' textAlign='left'>
             <Link to='your-teams'>
               <text>Edit Teams Page</text>
             </Link>
@@ -73,12 +122,19 @@ class HelpPage extends React.Component {
               Here you can leave, delete, invite, and recruit for your team!
             </b>
           </p>
-        </div>
-        <div style={{ paddingTop: '40px' }}>
+        </Accordion.Content>
+        <Accordion.Title
+          active={activeIndex === 4}
+          index={4}
+          onClick={this.handleClick}
+          >
+          <Icon name='dropdown' />
           <Header as='h1' textAlign='center'>
             <b>How do I Create a Team?</b>
           </Header>
-          <Header as='h2' textAlign='center'>
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 4}>
+        <Header as='h2' textAlign='left'>
             <Link to='create-team'>
               <text>Create Teams Page</text>
             </Link>
@@ -86,12 +142,19 @@ class HelpPage extends React.Component {
           <p>
             <b>Make sure to fill out the team creation form fully</b>
           </p>
-        </div>
-        <div style={{ paddingTop: '40px' }}>
+        </Accordion.Content>
+        <Accordion.Title
+          active={activeIndex === 5}
+          index={5}
+          onClick={this.handleClick}
+          >
+          <Icon name='dropdown' />
           <Header as='h1' textAlign='center'>
             <b>Can I be on Multiple Teams?</b>
           </Header>
-          <Header as='h2' textAlign='center'>
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 5}>
+        <Header as='h2' textAlign='left' color='green'>
             Yes!
           </Header>
           <p>
@@ -100,20 +163,35 @@ class HelpPage extends React.Component {
               allowed to join multiple teams.
             </b>
           </p>
-        </div>
+        </Accordion.Content>
+        </Accordion>
+        </Grid.Column>
+        <Grid.Column>
         <Divider horizontal style={{ paddingTop: '40px' }}>
           UNEXPECTED ERRORS
         </Divider>
-        <div style={{ paddingTop: '10px', paddingBottom: '30px' }}>
-          <Header as='h1' textAlign='center'>
-            <b>Site not Functioning Properly?</b>
-          </Header>
-          <Header as='h3' textAlign='center'>
+        <Accordion fluid styled exclusive={false}>
+            <Accordion.Title
+            active={activeIndex === 6}
+            index={6}
+            onClick={this.handleClick}
+            >
+            <Icon name='dropdown' />
+            <Header as='h1' textAlign='center'>
+              <b>Site not Functioning Properly?</b>
+            </Header>
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 6}>
+          <Header as='h3' textAlign='left'>
             Please screenshot the problem and direct message cmoore@hawaii.edu
             on Slack
           </Header>
-        </div>
-      </div>
+          </Accordion.Content>
+        </Accordion>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+    </Container>
     );
   }
 }
