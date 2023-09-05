d3869484872bc4f5cda2f70e5ed2bf35e796e8b9
Winston Co <winstonco@live.com>
2023-09-04 20:40:29 -1000

    set up tests and add landing page test

---
+++ app/tests/landingPage.e2e.js
@@ -0,0 +1,31 @@
+import { Selector } from 'testcafe';
+import { Credentials } from './tests.testcafe';
+
+class LandingPageTest {
+  constructor() {
+    this.pageId = '#landing-page';
+    this.pageSelector = Selector(this.pageId);
+  }
+
+  /** @type {(tc: TestController) => Promise<void>} */
+  async isDisplayed(tc) {
+    await tc.expect(this.pageSelector.exists).ok();
+    await tc.expect(this.pageSelector.visible).ok();
+  }
+
+  /** @type {(tc: TestController) => Promise<void>} */
+  async test(tc) {
+    // await tc.debug();
+    await this.isDisplayed(tc);
+  }
+
+  // IN CASE YOUR TEST WANTS CREDENTIALS, YOU CAN DO SOMETHING LIKE THIS
+  // /** @type {(tc: TestController, creds: Credentials) => void} */
+  // async test(tc, creds) {
+  //   // await tc.debug();
+  //   await this.isDisplayed(tc);
+  //   await this.somethingThatNeedsCreds(tc, creds);
+  // }
+}
+
+export const landingPageTest = new LandingPageTest();
---
+++ app/tests/tests.testcafe.js
@@ -0,0 +1,26 @@
+import {} from 'testcafe';
+import { landingPageTest } from './landingPage.e2e';
+
+/**
+ * @typedef {object} Credentials
+ * @property {string} username
+ * @property {string} password
+ */
+
+/** @type {Credentials} */
+const participantCredentials = {
+  username: 'john@foo.com',
+  password: 'changeme',
+};
+
+/** @type {Credentials} */
+const adminCredentials = {
+  username: 'admin@hacchui.ics.hawaii.edu',
+  password: 'changeme',
+};
+
+fixture('HACC-Hui').page('http://localhost:3400');
+
+test('Test landing page', async (testController) => {
+  await landingPageTest.test(testController);
+});
