2992071e79071ca8cc56874948b68db0c37c7762
Breadonn <braydonn@hawaii.edu>
2023-09-05 14:23:18 -1000

    Converted ViewPage component from class to fc

--- app/imports/ui/pages/administrator/ViewTeamsPage.jsx
+++ app/imports/ui/pages/administrator/ViewTeamsPage.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import withAllSubscriptions from '../../layouts/AllSubscriptionsHOC';
 import ViewTeams from '../../components/administrator/ViewTeams';
 
-class ViewTeamsPage extends React.Component {
+{/* class ViewTeamsPage extends React.Component {
   render() {
     return (
         <ViewTeams />
         );
   }
 }
+
+*/}
+
+ function ViewTeamsPage() {
+    return (
+      <ViewTeams />
+    );
+  }
 
 export default withAllSubscriptions(ViewTeamsPage);
