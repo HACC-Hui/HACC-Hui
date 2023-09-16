a08af439dbb4a170af3804a4a31b4cffd05878b4
Winston Co <winstonco@live.com>
2023-09-15 21:19:21 -1000

    fix signin oomponent

--- app/imports/ui/pages/Signin.jsx
+++ app/imports/ui/pages/Signin.jsx
@@ -1,132 +1,152 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useLocation } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
-import { Form, Button, Container, Row, Alert, Col } from 'react-bootstrap';
-import { Roles } from 'meteor/alanning:roles';
-import { ROLE } from '../../api/role/Role';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { FaEnvelope, FaLock } from 'react-icons/fa';
 import { Participants } from '../../api/user/ParticipantCollection';
 import { ROUTES } from '../../startup/client/route-constants';
 
-/**
- * Signin page overrides the form’s submit event and call Meteor’s loginWithPassword().
- * Authentication errors modify the component’s state to be displayed
- */
+/** @type { React.FC<{}> } */
+const Signin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [redirectToReferrer, setRedirectToReferrer] = useState(false);
 
-class Signin extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { email: '', password: '', error: '', redirectToReferer: false, role: '' };
-  }
+  /** @type {React.ChangeEventHandler<HTMLInputElement>} */
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-  handleChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  }
+  /** @type {React.ChangeEventHandler<HTMLInputElement>} */
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-  submit = () => {
-    const { email, password } = this.state;
+  /** @type {React.FormEventHandler<HTMLFormElement>} */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
     Meteor.loginWithPassword(email, password, (err) => {
       if (err) {
-        this.setState({ error: err.reason });
-      } else {
-        let role = ROLE.PARTICIPANT;
-        if (Roles.userIsInRole(Meteor.userId(), ROLE.ADMIN)) {
-          role = ROLE.ADMIN;
-        }
-        this.setState({ error: '', redirectToReferer: true, role: role });
+        setError(err.reason);
+        return;
       }
+
+      setError('');
+      setRedirectToReferrer(true);
     });
-  }
+  };
 
-  render() {
-    let pathname = ROUTES.LANDING;
-    if (Participants.isDefined(Meteor.userId())) {
-      const dev = Participants.findDoc({ userID: Meteor.userId() });
-      if (dev.isCompliant) {
-        if (dev.editedProfile) {
-          pathname = ROUTES.LANDING;
-        } else {
-          pathname = ROUTES.CREATE_PROFILE;
-        }
+  let pathname = ROUTES.LANDING;
+  if (Participants.isDefined(Meteor.userId())) {
+    const dev = Participants.findDoc({ userID: Meteor.userId() });
+    if (dev.isCompliant) {
+      if (dev.editedProfile) {
+        pathname = ROUTES.LANDING;
       } else {
-        pathname = ROUTES.AGE_CONSENT;
+        pathname = ROUTES.CREATE_PROFILE;
       }
+    } else {
+      pathname = ROUTES.AGE_CONSENT;
     }
-    const { from } = this.props.location.state || { from: { pathname } };
-
-    if (this.state.redirectToReferer) {
-      return <Redirect to={from} />;
-    }
-
-    return (
-
-        <Container>
-          <Row className="justify-content-center align-items-center">
-            <Col md={6}>
-              <h2 style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center' }}>
-                Login to your account</h2>
-              <Form onSubmit={this.submit}
-                    style={{
-                      display: 'grid',
-                      flexDirection: 'column',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-              >
-                <Form.Group style={{
-                  marginBottom: 10,
-                }}>
-                  <Form.Label>Email</Form.Label>
-                  <div className="input-group" >
-                    <div className="input-group-prepend">
-                      <span className="input-group-text"><i className="fas fa-envelope"></i></span>
-                    </div>
-                    <Form.Control
-                        type="email"
-                        name="email"
-                        placeholder="E-mail address"
-                        onChange={this.handleChange}
-                    />
-                  </div>
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Password</Form.Label>
-                  <div className="input-group">
-                    <div className="input-group-prepend">
-                      <span className="input-group-text"><i className="fas fa-lock"></i></span>
-                    </div>
-                    <Form.Control
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        onChange={this.handleChange}
-                    />
-                  </div>
-                </Form.Group>
-                <Button type="submit" variant="primary" style={{
-                  marginTop: 20,
-                  marginBottom: 10,
-                   }}>Submit</Button>
-              </Form>
-              {this.state.error === '' ? (
-                  ''
-              ) : (
-                  <Alert variant="danger">
-                    <strong>Login was not successful</strong>
-                    <p>{this.state.error}</p>
-                  </Alert>
-              )}
-            </Col>
-          </Row>
-        </Container>
-    );
   }
-}
+
+  const location = useLocation();
+
+  const { from } = location.state || { from: { pathname } };
+  if (redirectToReferrer) {
+    return <Redirect to={from} />;
+  }
+
+  return (
+    <Container id="signin-page">
+      <Row className="justify-content-center align-items-center">
+        <Col md={6}>
+          <h2
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            Login to your account
+          </h2>
+          <Form
+            onSubmit={handleSubmit}
+            style={{
+              display: 'grid',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Form.Group
+              style={{
+                marginBottom: 10,
+              }}
+            >
+              <Form.Label>Email</Form.Label>
+              <InputGroup>
+                <InputGroup.Text>
+                  <FaEnvelope />
+                </InputGroup.Text>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="E-mail address"
+                  value={email}
+                  onChange={handleChangeEmail}
+                />
+              </InputGroup>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <InputGroup>
+                <InputGroup.Text>
+                  <FaLock />
+                </InputGroup.Text>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={handleChangePassword}
+                />
+              </InputGroup>
+            </Form.Group>
+            <Button
+              type="submit"
+              variant="primary"
+              style={{
+                marginTop: 20,
+                marginBottom: 10,
+              }}
+            >
+              Submit
+            </Button>
+          </Form>
+          {error === '' ? (
+            ''
+          ) : (
+            <Alert variant="danger">
+              <strong>Login was not successful</strong>
+              <p>{error}</p>
+            </Alert>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 Signin.propTypes = {
   location: PropTypes.object,
