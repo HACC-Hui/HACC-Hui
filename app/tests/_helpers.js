e9f4f99cd6f650d143ad3ded8f3c7bb742a36872
Winston Co <winstonco@live.com>
2023-09-15 21:04:31 -1000

    create tests for dump database page

---
+++ app/tests/_helpers.js
@@ -0,0 +1,30 @@
+import { Selector } from 'testcafe';
+
+/**
+ * @typedef {object} Credentials
+ * @property {string} email
+ * @property {string} password
+ */
+
+/** @type {Credentials} */
+export const participantCredentials = {
+  email: 'john@foo.com',
+  password: 'changeme',
+};
+
+/** @type {Credentials} */
+export const adminCredentials = {
+  email: 'admin@hacchui.ics.hawaii.edu',
+  password: 'changeme',
+};
+
+/** @type {(tc: TestController, creds: Credentials) => Promise<void>} */
+export const signInAs = async (tc, creds) => {
+  await tc.navigateTo('/#/signin');
+  const emailInput = Selector('input[type="email"]');
+  const passwordInput = Selector('input[type="password"]');
+  await tc.typeText(emailInput, creds.email);
+  await tc.typeText(passwordInput, creds.password);
+  await tc.click(Selector('button').withText('Submit'));
+  await tc.expect(Selector('#landing-page').visible).ok();
+};
