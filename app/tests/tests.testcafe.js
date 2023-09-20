0811eb4909cedb9b5523b9d5163468dca6b73351
Winston Co <winstonco@live.com>
2023-09-19 22:37:59 -1000

    add tests for open teams and list teams pages, fix signInAs helper function

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -7,6 +7,8 @@ import { notFoundPageTest } from './NotFoundPage.e2e';
 import { helpPageTest } from './HelpPage.e2e';
 import { addToolPageTest } from './addToolPage.e2e';
 import { dumpDatabasePageTest } from './dumpDatabasePage.e2e';
+import { openTeamsPageTest } from './openTeamsPage.e2e';
+import { listTeamsPageTest } from './listTeamsPage.e2e';
 
 fixture('HACC-Hui').page('http://localhost:3400');
 
@@ -36,4 +38,12 @@ test('Test Add Tool page', async (testController) => {
 
 test('Test dump database page', async (testController) => {
   await dumpDatabasePageTest.test(testController);
+});
+
+test('Test open teams page', async (testController) => {
+  await openTeamsPageTest.test(testController);
+});
+
+test('Test list teams page', async (testController) => {
+  await listTeamsPageTest.test(testController);
 });
