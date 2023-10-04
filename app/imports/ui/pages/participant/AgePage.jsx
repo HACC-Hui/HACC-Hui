6246a44ae104041dc24fa4acbd8e5ab4180ce14c
kairitanaka <km321@hawaii.edu>
2023-10-04 04:53:33 -1000

    Update AgePage.jsx

--- app/imports/ui/pages/participant/AgePage.jsx
+++ app/imports/ui/pages/participant/AgePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Col, Button } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import { ROUTES } from '../../../startup/client/route-constants';
 
 /**
@@ -12,17 +12,17 @@ class AgePage extends React.Component {
     return (
         <div style={{ backgroundColor: '#393B44' }}>
           <div align={'center'} style={{ backgroundColor: '#24252B' }}>
-            <Col inverted style={{ padding: '5rem 10rem 5rem 10rem' }} as={'h2'}>
+            <Container inverted style={{ color: 'white', padding: '5rem 10rem 5rem 10rem' }} as={'h2'}>
               Before we move onto making your profile, we need to verify your age.
               <br/>
               Are you 18 or over?
               <br/>
               <Button as={NavLink} activeClassName="active" exact to={ROUTES.PARTICIPATION}
-                  style={{ color: 'white', backgroundColor: '#393B44' }} content="Yes, I am." />
+                      style={{ color: 'white', backgroundColor: '#393B44' }}>Yes, I am.</Button>
               <br/>
               <Button as={NavLink} activeClassName="active" exact to={ROUTES.UNDERAGE_PARTICIPATION}
-                      style={{ color: 'white', backgroundColor: '#393B44' }} content="No, I am not." />
-            </Col>
+                      style={{ color: 'white', backgroundColor: '#393B44' }}>No, I am not.</Button>
+            </Container>
           </div>
         </div>
     );
