5e6db0507ac9afe984677fa8597062dfb48d0be8
ntomchak <55006670+ntomchak@users.noreply.github.com>
2023-10-04 07:00:08 -1000

    converted listsuggestions and added test

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -7,33 +7,12 @@ import { notFoundPageTest } from './NotFoundPage.e2e';
 import { helpPageTest } from './HelpPage.e2e';
 import { addToolPageTest } from './addToolPage.e2e';
 import { dumpDatabasePageTest } from './dumpDatabasePage.e2e';
+import { listSuggestionsPageTest } from './ListSuggestionsPage.e2e';
 
 fixture('HACC-Hui').page('http://localhost:3400');
 
-test('Test landing page', async (testController) => {
-  await landingPageTest.test(testController);
-});
 
-test('Test sign in page', async (testController) => {
-  await signinPageTest.test(testController);
-});
 
-test('Test sign out page', async (testController) => {
-  await signoutPageTest.test(testController);
-});
-
-test('Test not found page', async (testController) => {
-  await notFoundPageTest.test(testController);
-});
-
-test('Test Help Page', async (testController) => {
-  await helpPageTest.test(testController);
-});
-
-test('Test Add Tool page', async (testController) => {
-  await addToolPageTest.test(testController);
-});
-
-test('Test dump database page', async (testController) => {
-  await dumpDatabasePageTest.test(testController);
+test('Test List Suggestions page', async (testController) => {
+  await listSuggestionsPageTest.test(testController);
 });
